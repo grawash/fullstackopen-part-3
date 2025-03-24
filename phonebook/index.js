a26ec44292587093f8e1e673a639c46dab0d460f
grawash <67363329+grawash@users.noreply.github.com>
2025-03-24 19:17:00 +0400

    add solution for exercise 3.22

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
@@ -12,10 +14,10 @@ morgan.token('body', function (req) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-  
+
 
 app.get('/', (request, response) => {
-    response.send('hello world')
+  response.send('hello world')
 })
 
 app.get('/api/persons', (request, response) => {
@@ -25,15 +27,15 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date();
-    Person.find({}).then(persons => {
-      response.send(`<p>Phonebook has info for ${persons.length} people<p/><br/><p>${date.toString()}<p/>`)
-    })
+  const date = new Date()
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people<p/><br/><p>${date.toString()}<p/>`)
+  })
 
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
       if (person) {
         response.json(person)
@@ -46,7 +48,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
@@ -54,43 +56,45 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'Name or number missing' 
-      })
-    }
-    // const userFound = persons.find(per => per.name === body.name)
-    // if (userFound){
-    //     return response.status(400).json({ 
-    //         error: 'The name already exists in the phonebook' 
-    //       })
-    // }
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
     })
+  }
+  // const userFound = persons.find(per => per.name === body.name)
+  // if (userFound){
+  //     return response.status(400).json({
+  //         error: 'The name already exists in the phonebook'
+  //       })
+  // }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save()
+  person.save()
     .then(savedPerson => {
       response.send(savedPerson)
     })
     .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
-  const {name, number} = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
   Person.findById(request.params.id)
-    .then(person =>{
+    .then(person => {
       if(!person){
         return response.status(404).end()
       }
       person.name = name
       person.number = number
 
-      person.save().then(updatedPerson => {
-        response.json(updatedPerson)
-      })
+      person.save()
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+        .catch(error => next(error))
     })
     .catch(error => next(error))
 })
@@ -100,14 +104,14 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
+  } else if (error.name === 'ValidationError' || error.name === 'validatorError') {
     return response.status(400).json({ error: error.message })
-  } 
+  }
 
   next(error)
 }
 app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`app running on http://localhost:${PORT}`)
+  console.log(`app running on http://localhost:${PORT}`)
 })
--- phonebook/modules/person.js
+++ phonebook/modules/person.js
@@ -1,61 +1,62 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        validate:[
-            {
-                validator: function(v) {
-                    return /^[^a-zA-Z]*$/.test(v);
-                },
-                message: 'must not contain any alphabetic characters (A-Z or a-z).'
-            },
-            {
-               validator: function(v) {
-                   const numberSections = v.split('-')
-                   return numberSections.length === 2
-               },
-               message: 'must contain exactly one "-" separating two parts.'
-            },
-           {
-                validator: function(v) {
-                    const numberSections = v.split('-')
-                    const firstPartLength = numberSections[0].length;
-                    return firstPartLength >= 2 && firstPartLength <= 3;
- 
-                },
-                message: 'first part of number should be 2 or 3 digits long'
-            }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate:[
+      {
+        validator: function(v) {
+          return /^[^a-zA-Z]*$/.test(v)
+        },
+        message: 'must not contain any alphabetic characters (A-Z or a-z).'
+      },
+      {
+        validator: function(v) {
+          const numberSections = v.split('-')
+          return numberSections.length === 2
+        },
+        message: 'must contain exactly one "-" separating two parts.'
+      },
+      {
+        validator: function(v) {
+          const numberSections = v.split('-')
+          const firstPartLength = numberSections[0].length
+          return firstPartLength >= 2 && firstPartLength <= 3
 
-        ]
-    }
+        },
+        message: 'first part of number should be 2 or 3 digits long'
+      }
+
+    ]
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
