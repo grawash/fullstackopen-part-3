90013d442071e8e487a4c11cf290857735266e0d
grawash <67363329+grawash@users.noreply.github.com>
2025-03-24 17:24:22 +0400

    add solution for exercises 3.19-3.21

--- phonebook/index.js
+++ phonebook/index.js
@@ -53,7 +53,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
       return response.status(400).json({ 
@@ -71,9 +71,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
       response.send(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
@@ -98,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   } 
 
   next(error)
--- phonebook/modules/person.js
+++ phonebook/modules/person.js
@@ -14,8 +14,40 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate:[
+            {
+                validator: function(v) {
+                    return /^[^a-zA-Z]*$/.test(v);
+                },
+                message: 'must not contain any alphabetic characters (A-Z or a-z).'
+            },
+            {
+               validator: function(v) {
+                   const numberSections = v.split('-')
+                   return numberSections.length === 2
+               },
+               message: 'must contain exactly one "-" separating two parts.'
+            },
+           {
+                validator: function(v) {
+                    const numberSections = v.split('-')
+                    const firstPartLength = numberSections[0].length;
+                    return firstPartLength >= 2 && firstPartLength <= 3;
+ 
+                },
+                message: 'first part of number should be 2 or 3 digits long'
+            }
+
+        ]
+    }
 })
 
 personSchema.set('toJSON', {
